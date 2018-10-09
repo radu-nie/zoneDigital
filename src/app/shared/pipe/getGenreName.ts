b8777a41ec70b169daf92fff4d7ee9022ec4b523
Nie Radu <nie.radu@gmail.com>
2018-10-09 13:51:05 +0300

    packages tr fix for heroku

--- src/app/shared/pipe/getGenreName.ts
+++ src/app/shared/pipe/getGenreName.ts
@@ -1,14 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Genre } from '../common/genre';
-/*
- * Raise the value exponentially
- * Takes an exponent argument that defaults to 1.
- * Usage:
- *   value | exponentialStrength:exponent
- * Example:
- *   {{ 2 | exponentialStrength:10 }}
- *   formats to: 1024
-*/
+
 @Pipe({ name: 'getGenreName' })
 export class GetGenreNamePipe implements PipeTransform {
     transform(value: number, genreArr: Array<Genre>): string {
